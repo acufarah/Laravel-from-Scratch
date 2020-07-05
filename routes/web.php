13395031923271724bda979f0f253ca5df839263
Farah Khan <acufarah@Farahs-MacBook-Air.local>
2020-07-04 23:13:02 -0400

    Add crud routes to web.php

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('main');
+
+Route::get('products', function() {
+    return 'This is the list of products';
+})->name('products.index');
+
+Route::get('products/create', function() {
+    return 'A form to create a product';
+})->name('products.create');
+
+Route::post('products', function() {
+    //
+})->name('products.store');
+
+Route::get('products/{product}', function($product){
+    return "Shows a single product {$product}";
+})->name('products.show');
+
+Route::get('products/{product}/edit', function($product) {
+    return "The form to edit product {$product}";
+})->name('products.edit');
+
+Route::match(['put','patch'], 'products/{product}', function($product) {
+    //
+})->name('products.update');
+
+Route::delete('products/{product', function($product) {
+    //
+})->name('products.destroy');
