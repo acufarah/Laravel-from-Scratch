3d4b0e729a6670c7e5bf0b71cd448b0bc91f01f1
Farah Khan <acufarah@Farahs-MacBook-Air.local>
2020-07-05 20:25:28 -0400

    Seed product db using a factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $products = factory(Product::class, 50)->create();
+    
     }
 }
