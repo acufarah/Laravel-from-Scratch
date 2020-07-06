13948ebb650da833c8759b3ad5405d8767731352
acufarah <farahkhanu@gmail.com>
2020-07-06 18:18:48 -0400

    Refactor into layouts and make create product form

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+@section('content')
+    <h1>Create a Product</h1>
+    <form method="POST" action="{{ route('products.store') }}">
+        <div class="form-row">
+            <label>Title</label>
+            <input class="form-control" type="text" name="title" required>
+        </div>
+        <div class="form-row">
+            <label>Description</label>
+            <input class="form-control" type="text" name="description" required>
+        </div>
+        <div class="form-row">
+            <label>Price</label>
+            <input class="form-control" type="number" min="1.00" step="0.01" name="price" required>
+        </div>
+        <div class="form-row">
+            <label>Stock</label>
+            <input class="form-control" type="number" min="0" name="stock" required>
+        </div>
+        <div class="form-row">
+            <label>Status</label>
+            <select class="custom-select" name="status" required>
+                <option value="" selected >Select...</option>
+                <option value="available" >Available</option>
+                <option value="unavailable" >Unavailable</option>
+            </select>
+        </div>
+    
+    </form>
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,97 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Document</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>List of Products</h1>
-        @empty ($products)
-            <div class="alert alert-warning">list is empty</div>
-        @else
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead class="thead-light">
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Stock</th>
-                    <th>Status</th>
-                </thead>
-                <tbody>
-                @foreach ($products as $product)
-                    <tr>
-                        <td>{{$product->id}}</td>
-                        <td>{{$product->title}}</td>
-                        <td>{{$product->description}}</td>
-                        <td>{{$product->price}}</td>
-                        <td>{{$product->stock}}</td>
-                        <td>{{$product->status}}</td>
-                    </tr>
-                @endforeach
-                </tbody>   
-            </table>
-        </div>
-        @endif
-    </body>
-</html>
+@extends('layouts.master')
+@section('content')
+    <h1>List of Products</h1>
+    @empty ($products)
+        <div class="alert alert-warning">list is empty</div>
+    @else
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead class="thead-light">
+                <th>Id</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Stock</th>
+                <th>Status</th>
+            </thead>
+            <tbody>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->title}}</td>
+                    <td>{{$product->description}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->stock}}</td>
+                    <td>{{$product->status}}</td>
+                </tr>
+            @endforeach
+            </tbody>   
+        </table>
+    </div>
+    @endif
+@endsection
